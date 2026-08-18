471294c4586e7832e5e089290c35622ccad6326c
xlgp2171 <xlgp2171@gmail.com>
2026-08-18 17:48:11 +0800

    refactor: 装饰器IDE推断增强

--- src/piz_core/deco/valid.py
+++ src/piz_core/deco/valid.py
@@ -1,20 +1,23 @@
 """ 验证装饰器
 
-:version: 0.2.260814
+:version: 0.3.260818
 """
 from functools import wraps
-from typing import ParamSpec, TypeVar, Callable, Any
+from typing import TypeVar, Callable, Any, overload, cast
 
 from piz_core.setting import Settings
 
 
-# 捕获任意参数签名
-P = ParamSpec("P")
-# 捕获返回值类型
-T = TypeVar("T")
+F = TypeVar("F", bound=Callable[..., Any])
 
 
-def validate_types(target_func: Callable[P, T] | None = None, /, *, strict: bool = True) -> Callable[P, T]:
+@overload
+def validate_types(target_func: F, /) -> F: ...
+
+@overload
+def validate_types(target_func: None = None, /, *, strict: bool = True) -> Callable[[F], F]: ...
+
+def validate_types(target_func: F | None = None, /, *, strict: bool = True) -> Callable[[F], F] | F:
     """ 基于期望类型的自动验证参数类型装饰器
 
     :param target_func: 输入函数
@@ -22,10 +25,10 @@ def validate_types(target_func: Callable[P, T] | None = None, /, *, strict: bool
     :raises TypeError: 类型不匹配异常；参数类型不是约束类型异常
     :raises ValueError: 超出范围的异常；参数未找到异常
     """
-    def _decorator(func: Callable[P, T]) -> Callable[P, T]:
+    def _decorator(func: F) -> F:
         # 保留原函数的元数据装饰器
         @wraps(func)
-        def _wrapper(*args: P.args, **kwargs: P.kwargs) -> Any:
+        def _wrapper(*args, **kwargs) -> F:
             # 全局开关
             if Settings.validate_types_enabled:
                 from piz_core.util import iter_arguments, validate_type, validate_constraint, get_func_path
@@ -37,6 +40,6 @@ def validate_types(target_func: Callable[P, T] | None = None, /, *, strict: bool
                     # 验证取值范围（针对Annotated类型）
                     validate_constraint(annotation, _name, arguments, error_hint=f",\tfunc: {get_func_path(func)}")
             return func(*args, **kwargs)
-        return _wrapper
+        return cast(F, _wrapper)
     # 如果target_func不是None，说明是直接使用无参装饰器
     return _decorator(target_func) if target_func is not None else _decorator
